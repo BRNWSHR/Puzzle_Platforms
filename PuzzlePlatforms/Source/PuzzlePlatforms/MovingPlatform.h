aaa651809fcbbf31869132d6784b3d21472b65d5
Alban JAMET <alban.jamet@gmail.com>
2018-07-19 10:51:54 +0200

    S02 - Connection made

---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MovingPlatform.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "MovingPlatform.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API AMovingPlatform : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	AMovingPlatform();
+
+	UPROPERTY(EditAnywhere)
+	float PlatformSpeed = 20;
+
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true))
+	FVector TargetLocation;
+
+	void AddActiveTrigger();
+	void RemoveActiveTrigger();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	FVector GlobalTargetLocation;
+	FVector GlobalStartLocation;
+
+	UPROPERTY(EditAnywhere)
+	int ActiveTriggers = 0;
+
+	UPROPERTY(EditAnywhere)
+	bool bDrivenByTrigger = false;
+	
+};
